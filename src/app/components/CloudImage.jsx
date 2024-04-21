c8847603e38022f9e1a1be3b2a7ef6178ca1950d
Adam Knapp <aknapp124@gmail.com>
2024-04-20 21:29:11 -0700

    Applied initial styles to images component

--- src/app/components/CloudImage.jsx
+++ src/app/components/CloudImage.jsx
@@ -5,5 +5,5 @@ import { CldImage } from 'next-cloudinary';
 import React from 'react';
 
 export default function CloudImage({ public_id }) {
-	return <CldImage width='250' height='400' src={public_id} alt='image' />;
+	return <CldImage width={100} height={150} src={public_id} alt='image' />;
 }
--- src/app/images/page.jsx
+++ src/app/images/page.jsx
@@ -1,5 +1,7 @@
 import CloudImage from '../components/CloudImage';
 
+import styles from './images.module.css';
+
 async function getImages() {
 	const res = await fetch('http://localhost:3000/api/images');
 
@@ -10,13 +12,21 @@ async function getImages() {
 const Images = async () => {
 	const images = await getImages();
 	return (
-		<main>
-			{images &&
-				images.map((image) => {
-					return (
-						<CloudImage public_id={image.public_id} key={image.asset_id} />
-					);
-				})}
+		<main className={styles.container}>
+			<h1>Images</h1>
+			<div className={styles.pictureBox}>
+				{images &&
+					images.map((image) => {
+						return (
+							<div>
+								<CloudImage
+									public_id={image.public_id}
+									key={image.asset_id}
+								/>
+							</div>
+						);
+					})}
+			</div>
 		</main>
 	);
 };
