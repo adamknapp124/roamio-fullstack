d158a7271afebffe57186fd4c216e8ebfa8f74e2
Adam Knapp <aknapp124@gmail.com>
2024-04-21 22:18:51 -0700

    Added grid to images component

--- src/app/images/page.jsx
+++ src/app/images/page.jsx
@@ -10,17 +10,40 @@ async function getImages() {
 	return data.data;
 }
 
+async function formatDate(date) {
+	const dateToFormat = date;
+	const newDate = new Date(dateToFormat);
+
+	// change timezone to users timezone
+	const options = {
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+		hour: '2-digit',
+		minute: '2-digit',
+		second: '2-digit',
+		timeZone: 'PST',
+	};
+	const formattedDate = newDate.toLocaleString('en-US', options);
+	return formattedDate;
+}
+
 const Images = async () => {
 	const images = await getImages();
 	return (
-		<main className={styles.container}>
+		<main>
 			<h1>Images</h1>
-			<div className={styles.pictureBox}>
+			<div className={styles.gridContainer}>
 				{images &&
 					images.map((image) => {
 						return (
-							<div key={image.asset_id}>
+							<div key={image.asset_id} className={styles.gridItem}>
 								<CloudImage public_id={image.public_id} />
+								<div>{formatDate(image.created_at)}</div>
+								<div className={styles.optionsBox}>
+									<button>Delete</button>
+									<button>Lock</button>
+								</div>
 							</div>
 						);
 					})}
