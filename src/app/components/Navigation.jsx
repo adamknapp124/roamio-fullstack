9f0ca97444cbbca1e5f132fdba75d47811e30af9
Adam Knapp <aknapp124@gmail.com>
2024-04-21 19:30:47 -0700

    Added map icon to navigation and scaffolded maps component

--- src/app/components/Navigation.jsx
+++ src/app/components/Navigation.jsx
@@ -4,6 +4,7 @@ import camera from '../../../public/icons/camera-svgrepo-com.svg';
 import shutter from '../../../public/icons/diaphragm.png';
 import dashboard from '../../../public/icons/dashboard-svgrepo-com.svg';
 import images from '../../../public/icons/images-svgrepo-com.svg';
+import map from '../../../public/icons/treasure-map.png';
 import Image from 'next/image';
 
 export const Navigation = () => {
@@ -12,17 +13,22 @@ export const Navigation = () => {
 			<div className={styles.separator}>
 				<div className={styles.navItem}>
 					<a href='/camera'>
-						<Image src={camera} alt='camera' height={50} width={50} />
+						<Image src={camera} alt='camera' height={35} width={35} />
+					</a>
+				</div>
+				<div className={styles.navItem}>
+					<a href='/map'>
+						<Image src={map} alt='map' height={35} width={35} />
 					</a>
 				</div>
 				<div className={styles.navItem}>
 					<a href='/dashboard'>
-						<Image src={dashboard} alt='dashboard' height={50} width={50} />
+						<Image src={dashboard} alt='dashboard' height={35} width={35} />
 					</a>
 				</div>
 				<div className={styles.navItem}>
 					<a href='/images'>
-						<Image src={images} alt='images' height={50} width={50} />
+						<Image src={images} alt='images' height={35} width={35} />
 					</a>
 				</div>
 			</div>
---
+++ src/app/map/page.jsx
@@ -0,0 +1,5 @@
+import styles from './map.module.css';
+
+export default function Map() {
+	return <div>Maps</div>;
+}
