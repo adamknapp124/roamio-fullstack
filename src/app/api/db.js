748452565e56df4c167dcb39620fab831b971ee9
Adam Knapp <aknapp124@gmail.com>
2024-05-15 19:39:08 -0700

    changed envir vars for testing

--- src/app/api/db.js
+++ src/app/api/db.js
@@ -2,10 +2,10 @@ import mysql from 'mysql2/promise';
 
 export async function dbConnection() {
 	const connection = await mysql.createConnection({
-		host: process.env.DB_HOST,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASSWORD,
-		database: process.env.DB_DATABASE,
+		host: 'localhost',
+		user: 'root',
+		password: 'Talon511!',
+		database: 'road_trip',
 	});
 	return connection;
 }
