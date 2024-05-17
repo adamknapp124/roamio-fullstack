7ed989add708566ff3e65c7ee1c57f591b31d519
Adam Knapp <aknapp124@gmail.com>
2024-05-16 19:41:49 -0700

    updated ep

--- src/app/globalActions.js
+++ src/app/globalActions.js
@@ -3,13 +3,13 @@
 import { cookies } from 'next/headers';
 
 export async function getUsers() {
-	const res = await fetch('https://35.94.175.53:/api/users');
+	const res = await fetch('http://35.94.175.53:80/api/users');
 	const data = await res.json();
 	return data.data;
 }
 
 export async function getImages() {
-	const res = await fetch('https://35.94.175.53/api/images');
+	const res = await fetch('http://35.94.175.53:80/api/images');
 	const data = await res.json();
 	return data.data;
 }
