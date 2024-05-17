2af0cce1711c5e6b20a427e37decd85be55d5d45
Adam Knapp <aknapp124@gmail.com>
2024-05-16 19:37:56 -0700

    fixed user issues

--- src/app/api/db.js
+++ src/app/api/db.js
@@ -4,7 +4,7 @@ export async function dbConnection() {
 	console.log('connection requested');
 	const connection = await mysql.createConnection({
 		host: 'my-roamio-db.ct0qkum0i4un.us-west-2.rds.amazonaws.com',
-		user: admin,
+		user: 'admin',
 		password: 'md1k8xZ3SmlQeYXeuxnJ',
 		database: 'roamio',
 	});
