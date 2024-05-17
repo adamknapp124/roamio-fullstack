478429911f7c5f18f8e90d1be98e5796934d4472
Adam Knapp <aknapp124@gmail.com>
2024-05-16 22:27:47 -0700

    changed to promises

--- src/app/api/db.js
+++ src/app/api/db.js
@@ -1,8 +1,8 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-export function dbConnection() {
+export async function dbConnection() {
 	console.log('connection requested');
-	const connection = mysql.createConnection({
+	const connection = await mysql.createConnection({
 		host: 'my-roamio-db.ct0qkum0i4un.us-west-2.rds.amazonaws.com',
 		user: 'admin',
 		database: 'roamio',
