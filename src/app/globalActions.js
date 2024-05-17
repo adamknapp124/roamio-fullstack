b1cc692de8d3decb2a49718eab0e0928bfaadd7e
Adam Knapp <aknapp124@gmail.com>
2024-05-16 19:36:09 -0700

    changed to http

--- src/app/globalActions.js
+++ src/app/globalActions.js
@@ -3,13 +3,13 @@
 import { cookies } from 'next/headers';
 
 export async function getUsers() {
-	const res = await fetch('https://35.94.175.53:3000/api/users');
+	const res = await fetch('http://35.94.175.53:3000/api/users');
 	const data = await res.json();
 	return data.data;
 }
 
 export async function getImages() {
-	const res = await fetch('https://35.94.175.53:3000/api/images');
+	const res = await fetch('http://35.94.175.53:3000/api/images');
 	const data = await res.json();
 	return data.data;
 }
