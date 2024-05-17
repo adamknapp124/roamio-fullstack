68c66fa4cf4b8e61149d62d3c72dc73910aae852
Adam Knapp <aknapp124@gmail.com>
2024-05-16 19:10:28 -0700

    testing endpoints

--- src/app/globalActions.js
+++ src/app/globalActions.js
@@ -3,23 +3,13 @@
 import { cookies } from 'next/headers';
 
 export async function getUsers() {
-	// Fetch users from the Database
-	const res = await fetch('/api/users');
-	// Parse the response
+	const res = await fetch('https://35.94.175.53:3000/api/users');
 	const data = await res.json();
-	// Return the data
 	return data.data;
 }
 
 export async function getImages() {
-	const res = await fetch('/api/images');
+	const res = await fetch('https://35.94.175.53:3000/api/images');
 	const data = await res.json();
 	return data.data;
-}
-
-export async function getUserById(id) {
-	const res = await fetch('/api/users/:id');
-	const data = await res.json();
-	const user = data.data;
-	return user;
 }
