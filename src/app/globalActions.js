9288a50067eb604fea8a9066b6eafc1a78fae101
Adam Knapp <aknapp124@gmail.com>
2024-05-22 16:42:19 -0700

    reverted IP

--- src/app/globalActions.js
+++ src/app/globalActions.js
@@ -7,7 +7,7 @@ export async function getUsers() {
 }
 
 export async function getImages() {
-	const res = await fetch('http://54.201.11.28:3000/api/images');
+	const res = await fetch('http://34.212.20.71:3000/api/images');
 	const data = await res.json();
 	return data.data;
 }
