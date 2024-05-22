219390dcf3e61c3ab9d8cb459ccda8b7a92c5d77
Adam Knapp <aknapp124@gmail.com>
2024-05-22 16:39:28 -0700

    switched ip for new instance

--- src/app/globalActions.js
+++ src/app/globalActions.js
@@ -1,13 +1,13 @@
 'use server';
 
 export async function getUsers() {
-	const res = await fetch('http://34.212.20.71:3000/api/users');
+	const res = await fetch('http://54.201.11.28:3000/api/users');
 	const data = await res.json();
 	return data.data;
 }
 
 export async function getImages() {
-	const res = await fetch('http://34.212.20.71:3000/api/images');
+	const res = await fetch('http://54.201.11.28:3000/api/images');
 	const data = await res.json();
 	return data.data;
 }
